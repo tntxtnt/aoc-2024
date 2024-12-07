e05afa50cbf988d80fde4a2537aee1a63dfa90e6
Tri Tran <lepnit205@gmail.com>
2024-12-07 07:23:57 -0600

    Day 7

---
+++ day07/main/src/day07_main.cpp
@@ -0,0 +1,84 @@
+#include <day07_solution.h>
+#define DOCTEST_CONFIG_IMPLEMENT
+#include <doctest/doctest.h>
+#include <sstream>
+#include <fstream>
+#include <fmt/ostream.h>
+#include <fmt/color.h>
+#include <aoc_session.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+void setVirtualTerminal() {
+#ifdef _WIN32
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD dwMode = 0;
+    GetConsoleMode(hOut, &dwMode);
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(hOut, dwMode);
+#endif
+}
+
+TEST_CASE("day07 - part 1") {
+    std::istringstream iss{R"(190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+)"};
+    const Day7Solution::Part1ResultType ans{3749};
+    Day7Solution sol;
+    const auto res = sol.part1(iss);
+    REQUIRE(res == ans);
+}
+
+TEST_CASE("day07 - part 2") {
+    std::istringstream iss{R"(190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+)"};
+    const Day7Solution::Part2ResultType ans{11387};
+    Day7Solution sol;
+    const auto res = sol.part2(iss);
+    REQUIRE(res == ans);
+}
+
+int main(int argc, char** argv) {
+    setVirtualTerminal();
+    doctest::Context context;
+    context.applyCommandLine(argc, argv);
+    const int doctestReturnCode = context.run();
+    if (context.shouldExit() || doctestReturnCode != EXIT_SUCCESS) return doctestReturnCode;
+
+    int returnCode = 0;
+    AdventOfCodeSession session(2024, 7);
+    if (session.fetchInput()) {
+        if (std::ifstream ifs{session.inputFileName}) {
+            Day7Solution sol;
+            const auto res1 = sol.part1(ifs);
+            session.checkAnswer(1, std::to_string(res1));
+
+            ifs.clear();
+            ifs.seekg(0);
+            const auto res2 = sol.part2(ifs);
+            session.checkAnswer(2, std::to_string(res2));
+        } else {
+            fmt::println(std::cerr, "{}",
+                         fmt::styled("Input file doesn't exists or can't be read", fg(fmt::color::red)));
+            returnCode = 1;
+        }
+    }
+    return doctestReturnCode + returnCode;
+}
---
+++ day07/solution/src/day07_solution.cpp
@@ -0,0 +1,52 @@
+#include <day07_solution.h>
+
+#include <sstream>
+#include <vector>
+#include <span>
+#include <ranges>
+namespace views = std::views;
+#include <fmt/format.h>
+
+auto Day7Solution::part1(std::istream& is) -> Part1ResultType {
+    Part1ResultType res{};
+    for (std::string line; std::getline(is, line);) {
+        std::istringstream iss{line};
+        long long target{};
+        std::vector<long long> nums;
+        iss >> target;
+        iss.ignore();
+        for (long long n; iss >> n;) nums.push_back(n);
+        auto canBeEvalTo = [](this auto&& canBeEvalTo, std::span<long long> nums, long long target,
+                              long long total) -> bool {
+            if (total > target) return false;
+            if (nums.empty()) return total == target;
+            return canBeEvalTo(nums | views::drop(1), target, total + nums[0]) ||
+                   canBeEvalTo(nums | views::drop(1), target, total * nums[0]);
+        };
+        if (canBeEvalTo(nums | views::drop(1), target, nums[0])) res += target;
+    }
+    return res;
+}
+
+auto Day7Solution::part2(std::istream& is) -> Part2ResultType {
+    Part2ResultType res{};
+    for (std::string line; std::getline(is, line);) {
+        std::istringstream iss{line};
+        long long target{};
+        std::vector<long long> nums;
+        iss >> target;
+        iss.ignore();
+        for (long long n; iss >> n;) nums.push_back(n);
+        auto canBeEvalTo = [](this auto&& canBeEvalTo, std::span<long long> nums, long long target,
+                              long long total) -> bool {
+            auto concat = [](long long a, long long b) { return std::stoll(fmt::format("{}{}", a, b)); };
+            if (total > target) return false;
+            if (nums.empty()) return total == target;
+            return canBeEvalTo(nums | views::drop(1), target, total + nums[0]) ||
+                   canBeEvalTo(nums | views::drop(1), target, total * nums[0]) ||
+                   canBeEvalTo(nums | views::drop(1), target, concat(total, nums[0]));
+        };
+        if (canBeEvalTo(nums | views::drop(1), target, nums[0])) res += target;
+    }
+    return res;
+}
