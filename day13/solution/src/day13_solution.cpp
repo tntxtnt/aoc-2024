6aba3be563214ba2ebb2729a49f90653cb632c43
Tri Tran <lepnit205@gmail.com>
2024-12-13 20:28:55 -0600

    Day 13 improvment

--- day13/solution/src/day13_solution.cpp
+++ day13/solution/src/day13_solution.cpp
@@ -2,142 +2,91 @@
 
 #include <vector>
 #include <string>
-#include <unordered_map>
+#include <variant>
+#include <numeric>
 #include <ranges>
 namespace ranges = std::ranges;
 namespace views = std::views;
 #include <aoc/utils.h>
 
-auto Day13Solution::part1(std::istream& inputStream) -> Part1ResultType {
-    Part1ResultType res{};
+template <typename T>
+concept Integer = std::is_integral_v<T>;
+
+template <Integer T>
+struct Point2i {
+    T x;
+    T y;
+};
+
+template <Integer T>
+struct Line2i { // ax + by = c
+    T a;
+    T b;
+    T c;
+};
+
+template <Integer T>
+static auto intersection(const Line2i<T>& line1,
+                         const Line2i<T>& line2) -> std::variant<std::nullptr_t, Point2i<T>, Line2i<T>> {
+    const T det = line1.a * line2.b - line1.b * line2.a;
+    const T dx = line1.c * line2.b - line1.b * line2.c;
+    const T dy = line1.a * line2.c - line1.c * line2.a;
+    if (det == 0) {
+        if (dx == 0 && dy == 0) return line1;
+        return nullptr;
+    }
+    if (dx % det == 0 && dy % det == 0) return Point2i<T>{dx / det, dy / det};
+    return nullptr;
+}
+
+template <class... Ts>
+struct overloaded : Ts... {
+    using Ts::operator()...;
+};
+
+static auto extractFirstInt(const std::string& str, char prefix) -> int {
+    const auto prefixIndex = str.find(prefix);
+    return std::stoi(str.substr(prefixIndex + 1, str.find(',') - prefixIndex - 1));
+}
+
+static auto extractLastInt(const std::string& str, char prefix) -> int {
+    return std::stoi(str.substr(str.find_last_of(prefix) + 1));
+}
+
+template <Integer T>
+static auto solve(std::istream& inputStream, const T added) -> T {
+    T res{};
     for (std::string line1, line2, line3;
-         std::getline(inputStream, line1) && std::getline(inputStream, line2) && std::getline(inputStream, line3);) {
-        // Ax + By = C
-        // Dx + Ey = F
-        const int a = std::stoi(line1.substr(line1.find('+') + 1, line1.find(',') - line1.find('+') - 1));
-        const int b = std::stoi(line2.substr(line2.find('+') + 1, line2.find(',') - line2.find('+') - 1));
-        const int c = std::stoi(line3.substr(line3.find('=') + 1, line3.find(',') - line3.find('=') - 1));
-        // aoc::println("{}x + {}y = {}", a, b, c);
-        const int d = std::stoi(line1.substr(line1.find_last_of('+') + 1));
-        const int e = std::stoi(line2.substr(line2.find_last_of('+') + 1));
-        const int f = std::stoi(line3.substr(line3.find_last_of('=') + 1));
-        // aoc::println("{}x + {}y = {}", d, e, f);
-        // det = AE - BD
-        // x = (CE - BF) / det
-        // y = (AF - CD) / det
-        const int det = a * e - b * d;
-        const int dx = c * e - b * f;
-        const int dy = a * f - c * d;
-        if (det == 0) {
-            // dx == dy == 0 or no solution
-            if (dx == 0 && dy == 0) { // AoC input doesn't contain such cases
-                // Ax + By = C
-                //    x = (C - By) / A
-                // or y = (C - Ax) / B
-                constexpr int maxRes = 3 * 101 + 101; // x, y <= 100
-                int minRes{maxRes};
-                int solX{};
-                int solY{};
-                for (int y = 1, n = c - b; y <= 100 && n >= 0; ++y, n -= b) {
-                    if (n % a == 0 && n / a <= 100) {
-                        const int val = 3 * (n / a) + y;
-                        if (val < minRes) {
-                            minRes = val;
-                            solX = n / a;
-                            solY = y;
-                        }
-                    }
-                }
-                for (int x = 1, n = c - a; x <= 100 && n >= 0; ++x, n -= a) {
-                    if (n % b == 0 && n / a <= 100) {
-                        const int val = 3 * x + n / b;
-                        if (val < minRes) {
-                            minRes = val;
-                            solX = x;
-                            solY = n / b;
-                        }
-                    }
-                }
-                aoc::println("{}x + {}y = {}", a, b, c);    // these lines
-                aoc::println("{}x + {}y = {}", d, e, f);    // are
-                aoc::println("x = {}, y = {}", solX, solY); // never getting printed
-                res += minRes == maxRes ? 0 : minRes;
-            }
-        } else {
-            if (dx % det == 0 && dy % det == 0) {
-                const int x = dx / det;
-                const int y = dy / det;
-                // aoc::println("x = {}, y = {}", x, y);
-                res += 3 * x + y;
-            }
-        }
-        std::getline(inputStream, line1); // ignore empty line
+         std::getline(inputStream, line1) && std::getline(inputStream, line2) && std::getline(inputStream, line3);
+         std::getline(inputStream, line1)) {
+        Line2i<T> intLine1{extractFirstInt(line1, '+'), extractFirstInt(line2, '+'),
+                           extractFirstInt(line3, '=') + added};
+        Line2i<T> intLine2{extractLastInt(line1, '+'), extractLastInt(line2, '+'), extractLastInt(line3, '=') + added};
+        std::visit(overloaded{[&](std::nullptr_t) {},                                //
+                              [&](const Point2i<T>& pt) { res += 3 * pt.x + pt.y; }, //
+                              [&](const Line2i<T>& ln) {
+                                  const auto [a, b, c] = ln;
+                                  // Note: extended Euclid algorithm is faster
+                                  const auto g = std::gcd(a, b);
+                                  if (c % g != 0) return;
+                                  Point2i<T> p1;
+                                  for (p1.x = 0; (c - a * p1.x) % b != 0;) ++p1.x;
+                                  p1.y = (c - a * p1.x) / b;
+                                  Point2i<T> p2;
+                                  for (p2.y = 0; (c - b * p2.y) % a != 0;) ++p2.y;
+                                  p2.x = (c - b * p2.y) / a;
+                                  res += std::min(3 * p1.x + p1.y, 3 * p2.x + p2.y);
+                              }},
+                   intersection(intLine1, intLine2));
     }
     return res;
 }
 
+auto Day13Solution::part1(std::istream& inputStream) -> Part1ResultType {
+    return solve<Part1ResultType>(inputStream, 0);
+}
+
 auto Day13Solution::part2(std::istream& inputStream) -> Part2ResultType {
     constexpr long long added = 10'000'000'000'000;
-    Part2ResultType res{};
-    for (std::string line1, line2, line3;
-         std::getline(inputStream, line1) && std::getline(inputStream, line2) && std::getline(inputStream, line3);) {
-        // Ax + By = C
-        // Dx + Ey = F
-        const long long a = std::stoi(line1.substr(line1.find('+') + 1, line1.find(',') - line1.find('+') + 1));
-        const long long b = std::stoi(line2.substr(line2.find('+') + 1, line2.find(',') - line2.find('+') + 1));
-        const long long c = std::stoi(line3.substr(line3.find('=') + 1, line3.find(',') - line3.find('=') + 1)) + added;
-        // aoc::println("{}x + {}y = {}", a, b, c);
-        const long long d = std::stoi(line1.substr(line1.find_last_of('+') + 1));
-        const long long e = std::stoi(line2.substr(line2.find_last_of('+') + 1));
-        const long long f = std::stoi(line3.substr(line3.find_last_of('=') + 1)) + added;
-        // aoc::println("{}x + {}y = {}", d, e, f);
-        // det = AE - BD
-        // x = (CE - BF) / det
-        // y = (AF - CD) / det
-        const long long det = a * e - b * d;
-        const long long dx = c * e - b * f;
-        const long long dy = a * f - c * d;
-        if (det == 0) {
-            // dx == dy == 0 or no solution
-            if (dx == 0 && dy == 0) {
-                /*
-ax + by = c has integer solution only iff c % gcd(a, b) == 0
-
-c/b
-\
- \
-  \
-   \
-    \
-  x  c/a
-  +----+
-    dx
-slope = m = -a/b
-tokens_x0 = c/b
-tokens_y0 = 3c/a
-tokens = 3x + y
-       = 3(c/a - dx) + (c - a*(c/a - dx))/b   // y = (c - ax) / b
-       = 3c/a - 3*dx + a*dx/b
-       = 3c/a - 3*dx - m*dx
-       = tokens_y0 - dx(m + 3)
-
-m + 3 < 0 or m < -3 or a/b > 3: -(m + 3) > 0 or tokens >= tokens_y0 --> min tokens = tokens_y0 = 3c/a
-m + 3 = 0 or m = -3 or a/b = 3: -(m + 3) = 0 or tokens == tokens_y0 --> min tokens = tokens_y0 = 3c/a = c/b
-m + 3 > 0 or m > -3 or a/b < 3: -(m + 3) > 0 or tokens <= tokens_y0 --> min tokens = tokens_x0 = c/b
-                */
-                aoc::println("{}x + {}y = {}", a, b, c);
-                aoc::println("{}x + {}y = {}", d, e, f);
-                // too lazy to write code
-            }
-        } else {
-            if (dx % det == 0 && dy % det == 0) {
-                const long long x = dx / det;
-                const long long y = dy / det;
-                // aoc::println("x = {}, y = {}", x, y);
-                res += 3 * x + y;
-            }
-        }
-        std::getline(inputStream, line1); // ignore empty line
-    }
-    return res;
+    return solve<Part2ResultType>(inputStream, added);
 }
