7f58107b7a2ab8bde7bfe3143f2e8e5edb9987a5
Tri Tran <lepnit205@gmail.com>
2024-12-22 08:13:14 -0600

    Day 22 refactored + faster use vec instead of hmap

--- day22/solution/src/day22_solution.cpp
+++ day22/solution/src/day22_solution.cpp
@@ -10,46 +10,53 @@ namespace views = std::views;
 #include <aoc/utils.h>
 
 struct Pseudorandom {
-    unsigned long long seed{};
-
-    unsigned long long random() {
-        mix(seed * 64);
-        prune();
-        mix(seed / 32);
-        prune();
-        mix(seed * 2048);
-        prune();
-        return (unsigned long long)seed;
+    using value_type = unsigned;
+    value_type seed{};
+    auto random() -> value_type {
+        seed = (seed ^ (seed * 64ULL)) % 16777216;
+        seed = (seed ^ (seed / 32ULL)) % 16777216;
+        seed = (seed ^ (seed * 2048ULL)) % 16777216;
+        return seed;
     }
-
-private:
-    void prune() { seed %= 16777216; }
-    void mix(unsigned long long num) { seed ^= num; }
 };
+
+constexpr unsigned changeCount = 2000;
 
 auto Day22Solution::part1(std::istream& inputStream) -> Part1ResultType {
     Part1ResultType res{};
     for (unsigned n : views::istream<unsigned>(inputStream)) {
         Pseudorandom gen{n};
-        ranges::for_each(views::iota(0) | views::take(1999), [&](int /*unused*/) { gen.random(); });
+        ranges::for_each(views::iota(0) | views::take(changeCount - 1), [&](int /*unused*/) { gen.random(); });
         res += gen.random();
     }
     return res;
 }
 
+consteval auto powi(unsigned n, unsigned exp) -> unsigned {
+    unsigned res{1};
+    for (; exp > 0; exp >>= 1, n *= n)
+        if (exp % 2 == 1) res *= n;
+    return res;
+}
+
 auto Day22Solution::part2(std::istream& inputStream) -> Part2ResultType {
-    constexpr size_t changeCount = 2000;
-    std::unordered_map<unsigned, int> bigMap;
-    for (unsigned n : views::istream<unsigned>(inputStream)) {
+    constexpr unsigned patternSize = 4;
+    constexpr unsigned base = 10;
+    constexpr unsigned deltaMagnitude = base - 1;
+    constexpr unsigned deltaSize = 2 * deltaMagnitude + 1;
+    constexpr unsigned hashSize = powi(deltaSize, patternSize);
+    std::vector<int> bigMap(hashSize);
+    for (Pseudorandom::value_type n : views::istream<unsigned>(inputStream)) {
         Pseudorandom gen{n};
-        decltype(bigMap) map;
-        for (unsigned hash{}, prev{n % 10}; unsigned i : views::iota(1) | views::take(changeCount)) {
-            unsigned curr = gen.random() % 10;
-            hash = ((hash << 5) | (curr - prev + 9)) & 0xFFFFF;
-            if (i >= 4 && !map.contains(hash)) map[hash] = curr;
+        decltype(bigMap) map(hashSize, -1);
+        for (unsigned hash{}, prev{n % base}; unsigned i : views::iota(1) | views::take(changeCount)) {
+            unsigned curr = gen.random() % base;
+            hash = (hash * deltaSize + curr - prev + deltaMagnitude) % hashSize;
+            if (i >= patternSize && map[hash] == -1) map[hash] = (int)curr;
             prev = curr;
         }
-        for (auto& [key, val] : map) bigMap[key] += val;
+        for (auto [key, val] : views::enumerate(map))
+            if (val != -1) bigMap[key] += val;
     }
-    return ranges::max(bigMap | views::transform([](auto& kval) { return kval.second; }));
+    return ranges::max(bigMap);
 }
